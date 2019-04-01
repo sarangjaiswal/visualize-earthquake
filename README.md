475b7067b49c2a8cd3a742cbb95cc446507f3efc
Sarang Jaiswal <sarang.j@outlook.com>
2019-04-01 17:11:20 -0500

    Added Version 2.0 to Visualize earthquakes on Indian Sub Continent

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Visualize earthquake's in India
-A Project to Visualize history of earthquake's in India on Map. 
+# Visualize earthquake's on Indian Sub Continent between 2010 to 2019
+A Project to Visualize history of earthquake's on Indian Sub Continent on Map. 
 
 ## Installation
 
@@ -11,18 +11,49 @@ This is a **Python 3.6** module that depends on the **Folium** packages.
 4. Test the program by running `visualize-earthquake.py` 
 
 ## Usage
-Just run `visualize-earthquake.py` file. The output will be a html file `earthquake.html` under `\maps` folder. 
+Just run `visualize-earthquake.py` file. The output will be a html file `earthquake_india.html` under `\maps` folder. 
+
+Information on GeoJSON Detail Format can be found [here](https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson_detail.php)
 
 ## Data
-All data used in this example is sourced from internet.
+All data used in this example is sourced from below websites.
 * **USGS**
     * https://earthquake.usgs.gov/
-
+* **Mygeodata**
+    * https://mygeodata.cloud/r
+* **Shape files**
+    * https://github.com/fraxen/tectonicplates
 
 ## Map
 
+#### Version 2.0
+This is upgraded version of my previous attempt to visualize the earthquakes in Indian Sub-Continent. I have added the following to make it user friendly
+* **Added four layers to the visualization**
+    * Micro Plates & Major Fault Zones
+    * Plate Interface
+    * Plate Motion
+    * Earthquake Data
+* A**dded Colored Indicators to map**
+    * Color depends on the magnitude of earthquake.
+* **Tooltips**
+    * Hovering over the circles will display information
+* **Popup Information**
+    * When user clicks on any one of the circle following information are displayed
+        * Place of earthquake
+        * Magnitude of earthquake which is color coded too
+        * Date when the earthquake happened
+        * Type information
+
 ![Map](img/earthquake.png)
+#### Version 1.0
+This is a basic visualization of earthquake in Indian Sub-Continent. The Blue markers shown in the map are places where the earthquake occured between 2010 to 2019.
+![Map](img/earthquake_old.png)
 
-
+## References
+* [Modified Mercalli intensity scale](https://en.wikipedia.org/wiki/Modified_Mercalli_intensity_scale)
+* [Magnitude / Intensity Comparison](https://earthquake.usgs.gov/learn/topics/mag_vs_int.php)
+* [GIS Lounge](https://www.gislounge.com/find-tectonic-plate-gis-data/)
 ## Disclaimer
 All data used in this example is sourced from internet. The Author does not take guarantee of accuracy.  
+
+
--- visualize-earthquakes.py
+++ visualize-earthquakes.py
@@ -1,13 +1,55 @@
-# https://earthquake.usgs.gov/fdsnws/event/1/
-# https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minlatitude=3&minlongitude=37&maxlatitude=63&maxlongitude=98
-# https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minlatitude=3&minlongitude=37&maxlatitude=63&maxlongitude=98&starttime=2014-01-01&endtime=2019-01-02
-
+# importing library
 import folium
-
+import time
 import requests
+import geopandas as gpd
+import os
+
+# get current directory path
+cur_dir = os.path.dirname(os.path.realpath(__file__))
+
+# target path to save map
+map_path = os.path.join('maps', 'earthquake_india.html')
+
+# read shape files with geopandas
+Micro_Plates_and_Major_Fault_Zones = gpd.read_file(cur_dir + '/data/mygeodata/plate-boundaries/shp/Micro_Plates_and_Major_Fault_Zones-line.shp')
+Plate_Interface = gpd.read_file(cur_dir + '/data/mygeodata/plate-boundaries/shp/Plate_Interface-line.shp')
+Plate_Motion = gpd.read_file(cur_dir + '/data/mygeodata/plate-boundaries/shp/Plate_Motion-point.shp')
 
 # Open the map at a location with a certain zoom start - http://geojson.io/#map=5/22.573/74.751
 m = folium.Map(location=[22.573, 74.751], zoom_start=5)
+
+# creating feature groups which will be added to map as layers
+fg_zone = folium.FeatureGroup('Micro Plates & Major Fault Zones')
+fg_interface = folium.FeatureGroup('Plate Interface')
+fg_motion = folium.FeatureGroup('Plate Motion')
+fg_earthquake = folium.FeatureGroup('Earthquake Data')
+
+# adding micro plates and major fault zones data to feature group
+folium.Choropleth(
+    geo_data=Micro_Plates_and_Major_Fault_Zones,
+    line_color='red',
+    line_weight=2,
+    line_opacity=1,
+    highlight=True
+).add_to(fg_zone)
+
+# adding plate interface data to feature group
+folium.Choropleth(
+    geo_data=Plate_Interface,
+    line_color='red',
+    line_weight=2,
+    line_opacity=1,
+    highlight=True
+).add_to(fg_interface)
+
+# adding plate motion data to feature group
+folium.GeoJson(Plate_Motion).add_to(fg_motion)
+
+# adding the feature group to folium map
+fg_zone.add_to(m)
+fg_interface.add_to(m)
+fg_motion.add_to(m)
 
 # Read earthquake from past hour
 url_1hour = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
@@ -19,22 +61,70 @@ url_history = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&m
 r = requests.get(url_history)
 data = r.json()
 
+# extracted from https://en.wikipedia.org/wiki/Modified_Mercalli_intensity_scale
+color_list = {
+    1: '#ffffff',
+    2: '#bfccff',
+    3: '#99f',
+    4: '#8ff',
+    5: '#7df894',
+    6: '#ff0',
+    7: '#fd0',
+    8: '#ff9100',
+    9: '#f00'
+    }
 
+# getting the number of records in json
 count = data["metadata"]["count"]
-print(count)
 
-for i in range(0,count-1):
+# tool tip text
+tooltip = 'Click for more info.'
+
+# iterating all the json records
+for i in range(0, count-1):
     title = data["features"][i]["properties"]["title"]
     if title.find("India") != -1:
-        print(data["features"][i]["properties"]["title"])
+
+        # get the magnitude, round to nearest number and convert to int
+        mag = int(round(data["features"][i]["properties"]["mag"], 0))
+
+        # convert the time into readable format
+        event_time = time.strftime(
+                        '%Y-%m-%d %H:%M:%S',
+                        time.localtime(round(data["features"][i]["properties"]["time"]/1000, 10))
+                    )
+        # html format for pop ups
+        popup = f"""<div><strong>Place : </strong>{data["features"][i]["properties"]["place"]}</div>
+                    <div>
+                        <strong>Magnitude : </strong>
+                        <div style="background-color:{color_list.get(mag)}">
+                            {data["features"][i]["properties"]["mag"]}
+                        </div>
+                    </div>
+                    <div><strong>Date : </strong>{event_time}</div>
+                    <div><strong>Type : </strong>{data["features"][i]["properties"]["type"]}</div>
+                """
+        # get longitude and latitude from json data
         lon = data["features"][i]["geometry"]["coordinates"][0]
         lat = data["features"][i]["geometry"]["coordinates"][1]
-        folium.Marker([lat, lon], popup=data["features"][i]["properties"]["place"]).add_to(m)
 
-m.save('earthquake.html')
+        # Add a circles to feature group
+        folium.CircleMarker(
+            location=[lat, lon],
+            radius=5,
+            tooltip=tooltip,
+            popup=folium.Popup(popup, max_width=1000),
+            color=color_list.get(mag),
+            fill=True,
+            fill_color=color_list.get(mag)
+        ).add_to(fg_earthquake)
+
+# add feature group to map
+fg_earthquake.add_to(m)
 
 
+# add layer to map
+folium.LayerControl().add_to(m)
 
-
-
-
+# save map to specified location
+m.save(map_path)
