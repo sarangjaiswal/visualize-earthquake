cbfe5124e754c56420af2fe1732706a77204aa3b
Sarang Jaiswal <sarang.j@outlook.com>
2019-04-01 01:48:09 -0500

    Added visualization on India's history with earthquakes

---
+++ README.md
@@ -0,0 +1,28 @@
+# Visualize earthquake's in India
+A Project to Visualize history of earthquake's in India on Map. 
+
+## Installation
+
+This is a **Python 3.6** module that depends on the **Folium** packages.
+
+1. Clone and `cd` into this repo.
+2. Install **Python 3.6**.
+3. Install requirements from pip with `python3 -m pip install -r requirements.txt`.
+4. Test the program by running `visualize-earthquake.py` 
+
+## Usage
+Just run `visualize-earthquake.py` file. The output will be a html file `earthquake.html` under `\maps` folder. 
+
+## Data
+All data used in this example is sourced from internet.
+* **USGS**
+    * https://earthquake.usgs.gov/
+
+
+## Map
+
+![Map](img/earthquake.png)
+
+
+## Disclaimer
+All data used in this example is sourced from internet. The Author does not take guarantee of accuracy.  
--- visualize-earthquakes.py
+++ visualize-earthquakes.py
@@ -1,3 +1,40 @@
 # https://earthquake.usgs.gov/fdsnws/event/1/
 # https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minlatitude=3&minlongitude=37&maxlatitude=63&maxlongitude=98
 # https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minlatitude=3&minlongitude=37&maxlatitude=63&maxlongitude=98&starttime=2014-01-01&endtime=2019-01-02
+
+import folium
+
+import requests
+
+# Open the map at a location with a certain zoom start - http://geojson.io/#map=5/22.573/74.751
+m = folium.Map(location=[22.573, 74.751], zoom_start=5)
+
+# Read earthquake from past hour
+url_1hour = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson'
+url_1day = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson'
+url_7days = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson'
+url_30days = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
+url_history = 'https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minlatitude=3&minlongitude=37&maxlatitude=63&maxlongitude=98&starttime=2010-01-01&endtime=2019-01-02'
+
+r = requests.get(url_history)
+data = r.json()
+
+
+count = data["metadata"]["count"]
+print(count)
+
+for i in range(0,count-1):
+    title = data["features"][i]["properties"]["title"]
+    if title.find("India") != -1:
+        print(data["features"][i]["properties"]["title"])
+        lon = data["features"][i]["geometry"]["coordinates"][0]
+        lat = data["features"][i]["geometry"]["coordinates"][1]
+        folium.Marker([lat, lon], popup=data["features"][i]["properties"]["place"]).add_to(m)
+
+m.save('earthquake.html')
+
+
+
+
+
+
