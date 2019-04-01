c405621a306c9aa8a4ad35577d416535689e1675
Sarang Jaiswal <sarang.j@outlook.com>
2019-03-31 23:31:39 -0500

    Initial Commit

---
+++ visualize-earthquakes.py
@@ -0,0 +1,3 @@
+# https://earthquake.usgs.gov/fdsnws/event/1/
+# https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minlatitude=3&minlongitude=37&maxlatitude=63&maxlongitude=98
+# https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&minlatitude=3&minlongitude=37&maxlatitude=63&maxlongitude=98&starttime=2014-01-01&endtime=2019-01-02
